28d83f75dfb44f0621902603cbcd2a3b0a94bcb8
evrengit <evren.pehlivan@outlook.com>
2021-02-26 08:27:12 +0300

    .
    .

---
+++ day1/lib1.js
@@ -0,0 +1,23 @@
+ 
+//document.getElementById("demo2").innerHTML = "<span style='color:#778899'>just text</span>";
+
+window.alert(5 + 6);
+
+console.log(15 + 16);
+
+
+//var variable1="a"+2;
+
+var a=1;
+var a=2;
+a="evren"+'pehlivan';
+console.log(a);
+
+const name="evren";
+
+var b=
+"<span style='color:red'>"+name+"</span><br>"+
+"<span style='color:green'>"+name+"</span><br>"+
+"<span style='color:yellow'>"+name+"</span><br>";
+
+document.getElementById("demo").innerHTML = b;
---
+++ day2/lib1.js
@@ -0,0 +1,22 @@
+ 
+ 
+
+//var variable1="a"+2;
+
+var a=1;
+var a=2;
+a="evren"+'pehlivan';
+console.log(a);
+
+const name="evren";
+
+  
+
+const car = {type:"Fiat", model:"500", color:"white"};
+
+car.evrenproperty="yellow9";
+
+var $ = 5 ** 2;
+
+
+document.getElementById("demo").innerHTML = instanceof $;
